9e250e1022d4d4f59647732a38c7383e9774a961
Fumihiro Kato <fumi@fumi.me>
2012-02-16 22:27:00 +0900

    Added NGramAnalyzer14Factory.java

--- src/jp/linkeddata/owlim/NGramAnalyzer14Factory.java
+++ src/jp/linkeddata/owlim/NGramAnalyzer14Factory.java
@@ -4,8 +4,8 @@ import com.ontotext.trree.plugin.lucene.AnalyzerFactory;
 import org.apache.lucene.analysis.Analyzer;
 import jp.linkeddata.lucene.analysis.NGramAnalyzer;
 
-public class NGramAnalyzer3Factory implements AnalyzerFactory {
+public class NGramAnalyzer14Factory implements AnalyzerFactory {
     public Analyzer createAnalyzer(){
-        return new NGramAnalyzer(3, 3);
+        return new NGramAnalyzer(1, 4);
     }
 }
